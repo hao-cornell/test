47830adc52d737d94d90e0b340f86d38603414fb
Ming Hao <haoliang.cornell@gmail.com>
2022-05-03 17:38:32 -0700

    add some logic

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println((a+b)/2);
 	}
 }
